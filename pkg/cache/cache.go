c81447cac1b9a5550daa34c3d12dbf44193bbee0
Drausin Wulsin <daedalus2718@gmail.com>
2018-01-13 08:28:30 -0500

    add cache eviction (#3)
    * add eviction and tests
    
    * add cache params

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -1,21 +1,29 @@
 package cache
 
+import "time"
+
+const (
+	defaultReceptWindow         = time.Hour * 24 * 7
+	defaultEvictionBatchSize    = uint(100)
+	defaultEvictionPeriod       = 30 * time.Minute
+	defaultEvictionQueryTimeout = 5 * time.Second
+)
+
 // Cache stores documents in a quasi-LRU cache. Implementations of this interface define how the
 // storage layer works.
 type Cache interface {
-
 	// Put stores the marshaled document value at the hex of its key.
 	Put(key string, value []byte) error
 
-	// Get retrieves the marshaled document value of the given hex key.
+	// Get retrieves the marshaled document value of the given key.
 	Get(key string) ([]byte, error)
 
-	// TODO (drausin) eviction
+	// EvictNext removes the next batch of documents eligible for eviction from the cache.
+	EvictNext() error
 }
 
 // AccessRecorder records put and get access to a particular document.
 type AccessRecorder interface {
-
 	// CachePut creates a new access record with the cache's put time for the document with
 	// the given key.
 	CachePut(key string) error
@@ -23,8 +31,30 @@ type AccessRecorder interface {
 	// CacheGet updates the access record's latest get time for the document with the given key.
 	CacheGet(key string) error
 
+	// CacheEvict deletes the access record for the documents with the given keys.
+	CacheEvict(keys []string) error
+
 	// LibriPut updates the access record's latest libri put time.
 	LibriPut(key string) error
 
-	// TODO (drausin) docs to evict
+	// GetNextEvictions gets the next batch of keys for documents to evict.
+	GetNextEvictions() ([]string, error)
+}
+
+// Parameters defines the parameters used by the cache implementation.
+type Parameters struct {
+	RecentWindow         time.Duration
+	EvictionBatchSize    uint
+	EvictionPeriod       time.Duration
+	EvictionQueryTimeout time.Duration
+}
+
+// NewDefaultParameters returns a new instance of default cache parameter values.
+func NewDefaultParameters() *Parameters {
+	return &Parameters{
+		RecentWindow:         defaultReceptWindow,
+		EvictionBatchSize:    defaultEvictionBatchSize,
+		EvictionPeriod:       defaultEvictionPeriod,
+		EvictionQueryTimeout: defaultEvictionQueryTimeout,
+	}
 }
