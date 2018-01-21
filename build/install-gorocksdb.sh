ef35355bec2a71159826422afcac576117b37c0c
Drausin Wulsin <daedalus2718@gmail.com>
2018-01-21 11:22:32 -0500

    update install-gorocksdb script

--- build/install-gorocksdb.sh
+++ build/install-gorocksdb.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 CGO_CFLAGS="-I/usr/local/include/rocksdb" \
-CGO_LDFLAGS="-L/usr/local/lib/librocksdb.a -lrocksdb -lstdc++ -lm -lz -lbz2 -lsnappy" \
-go get github.com/tecbot/gorocksdb
+CGO_LDFLAGS="-L/usr/local/lib -lrocksdb -lstdc++ -lm -lz -lbz2 -lsnappy" \
+go install github.com/elxirhealth/courier/vendor/github.com/tecbot/gorocksdb
