a122b6e34ea92aa7cf2cd7306fbf0f985510b95b
Drausin Wulsin <daedalus2718@gmail.com>
2018-03-16 22:30:32 -0400

    add client package (#22)

---
+++ pkg/client/client.go
@@ -0,0 +1,15 @@
+package client
+
+import (
+	api "github.com/elxirhealth/courier/pkg/courierapi"
+	"google.golang.org/grpc"
+)
+
+// NewInsecure returns a new CourierClient without any TLS on the connection.
+func NewInsecure(address string) (api.CourierClient, error) {
+	cc, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	return api.NewCourierClient(cc), nil
+}
